2db4d90f8d38fd072c05d7d1338928fce06ac8e0
Aida Yrysbekova <aidiko20@gmail.com>
2019-08-06 01:47:14 -0400

    added json file

--- app/data/friends.js
+++ app/data/friends.js
@@ -1,32 +1,32 @@
 var friends = [
     {
         name: "Joe",
-        picture: "",
+        picture: "https://fnwonline.com/wp-content/uploads/2018/01/Joey-Tribbiani.png",
         scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
     },
     {
         name: "Phoebe",
-        picture: "",
+        picture: "https://media1.popsugar-assets.com/files/thumbor/L6u2EA_RHtV_piy8CDItMVqiUQ4/fit-in/2048xorig/filters:format_auto-!!-:strip_icc-!!-/2014/11/04/740/n/1922283/b873d68328a0eef2_Phoebe-Top.jpg",
         scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
     },
     {
         name: "Chandler",
-        picture: "",
+        picture: "http://images5.fanpop.com/image/photos/29600000/Matthew-Perry-matthew-perry-29661448-383-473.jpg",
         scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
     },
     {
         name: "Ross",
-        picture: "",
+        picture: "https://upload.wikimedia.org/wikipedia/en/6/6f/David_Schwimmer_as_Ross_Geller.jpg",
         scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
     },
     {
         name: "Monica",
-        picture: "",
+        picture: "https://vignette.wikia.nocookie.net/friends/images/2/2f/Monica_Geller-Bing_Season_10.png/revision/latest?cb=20180426182234",
         scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
     },
     {
         name: "Rachel",
-        picture: "",
+        picture: "http://images2.fanpop.com/images/photos/5300000/Rachel-Green-Friends-tv-characters-5385845-300-400.jpg",
         scores: [4, 2, 3, 5, 4, 3, 1, 4, 5, 4]
     }
 ];
