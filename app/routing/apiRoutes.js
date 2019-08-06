bbc5912ae88624de15a40027513b035b5a6cea49
Aida Yrysbekova <aidiko20@gmail.com>
2019-08-06 10:03:28 -0400

    fixed stylesheet

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,41 +1,41 @@
-var path = require("path");
+// var path = require("path");
 var friends = require("../data/friends.js");
 
-module.exports = function (app){
+module.exports = function (app) {
 
-app.get("/api/friends", function(req, res){
- res.json(friends);
-});
-app.post("/api/friends", function(req, res) {
-    var newFriendData = {
-        name: req.body.name,
-        picture: req.body.picture,
-        scores: []
-    };
-    var scoresArray = [];
-    for (var i=0; i , req.body.scores.length; i++){
-        scoresArray.push( parseInt(req.body.scores[i]))
-    }
-    newFriendData.scores = scoresArray;
- var compareScores = [];
- for (var i=0; i < friendsData.length; i++){
-     var compareCurrent = 0;
- for (var k=0; k < newFriendData.scores.length; k++){
-     compareCurrent += Math.abs(newFriendData[k] - friendsData[i].scores[k]);
- }
- compareScores.push(compareCurrent);
-}
-var yourMatch = 0;
-for (var i=1; i < compareScores.length; i++){
-    if(compareScores[i] <= compareScores[yourMatch]){
-        yourMatch = i;
-    }
-}
+    app.get("/api/friends", function (req, res) {
+        res.json(friends);
+    });
+    app.post("/api/friends", function (req, res) {
+        var newFriendData = {
+            name: req.body.name,
+            picture: req.body.picture,
+            scores: req.body.scores
+        };
+        // var scoresArray = [];
+        // for (var i = 0; i < req.body.scores.length; i++) {
+        //     scores.push(parseInt(req.body.scores[i]))
+        // }
+        // newFriendData.scores = scoresArray;
+        // console.log("This is the scores array: " + scoresArray)
+        // var compareScores = [];
+        // for (var i = 0; i < friendsData.length; i++) {
+        //     var compareCurrent = 0;
+        //     for (var j = 0; j < newFriendData.scores.length; j++) {
+        //         compareCurrent += Math.abs(newFriendData[j] - friendsData[i].scores[j]);
+        //         console.log(compareCurrent);
+        //     }
+        //     compareScores.push(compareCurrent);
+        // }
+        // var yourMatch = 0;
+        // for (var i = 1; i < compareScores.length; i++) {
+        //     if (compareScores[i] <= compareScores[yourMatch]) {
+        //         yourMatch = i;
+        //     }
+        // }
 
-var FriendMatch = friendsData[yourMatch];
-
-req.json(FriendMatch);
-
-friendsData.push(newFriendData);
-});
+        // var FriendMatch = friendsData[yourMatch];
+        friends.push(newFriendData);
+        // res.json(FriendMatch);
+    });
 }
