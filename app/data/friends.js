46d5363d9edcafb1208fb290c9102c885a9eb95a
Aida Yrysbekova <aidiko20@gmail.com>
2019-08-05 23:50:26 -0400

    added all the codes, had to delete lasr repository due to unknown issues

---
+++ app/data/friends.js
@@ -0,0 +1,33 @@
+var friends = [
+    {
+        name: "Joe",
+        picture: "",
+        scores: [3, 1, 4, 4, 2, 4, 2, 5, 1, 3]
+    },
+    {
+        name: "Phoebe",
+        picture: "",
+        scores: [3, 1, 3, 5, 2, 3, 2, 2, 5, 5]
+    },
+    {
+        name: "Chandler",
+        picture: "",
+        scores: [1, 1, 5, 3, 3, 5, 1, 2, 3, 4]
+    },
+    {
+        name: "Ross",
+        picture: "",
+        scores: [2, 5, 4, 1, 2, 3, 3, 2, 3, 3]
+    },
+    {
+        name: "Monica",
+        picture: "",
+        scores: [5, 1, 2, 3, 1, 2, 5, 1, 1, 1]
+    },
+    {
+        name: "Rachel",
+        picture: "",
+        scores: [4, 2, 3, 5, 4, 3, 1, 4, 5, 4]
+    }
+];
+module.exports = friends;
---
+++ app/routing/apiRoutes.js
@@ -0,0 +1,42 @@
+var path = require("path");
+var friends = require("../data/friends.js");
+
+module.exports = function (app){
+
+app.get("/api/friends", function(req, res){
+ res.json(friends);
+});
+app.post("/api/friends", function(req, res) {
+    var newFriendData = {
+        name: req.body.name,
+        picture: req.body.picture,
+        scores: []
+    };
+    var scoresArray = [];
+    for (var i=0; i , req.body.scores.length; i++){
+        scoresArray.push( parseInt(req.body.scores[i]))
+    }
+    newFriendData.scores = scoresArray;
+ var compareScores = [];
+ for (var i=0; i < friendsData.length; i++){
+     var compareCurrent = 0;
+ for (var k=0; k < newFriendData.scores.length; k++){
+     compareCurrent += Math.abs(newFriendData[k] - friendsData[i].scores[k]);
+ }
+ compareScores.push(compareCurrent);
+}
+var yourMatch = 0;
+for (var i=1; i < compareScores.length; i++){
+    if(compareScores[i] <= compareScores[yourMatch]){
+        yourMatch = i;
+    }
+}
+
+var FriendMatch = friendsData[yourMatch];
+
+req.json(FriendMatch);
+
+friendsData.push(newFriendData);
+});
+}
+module.exports = apiRoutes;
